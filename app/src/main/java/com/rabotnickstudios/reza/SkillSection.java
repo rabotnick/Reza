fe441278e2fe8b58452a0a51e9d0ac762e0040da
Austin <aenyidah@terpmail.umd.edu>
2015-01-18 01:43:06 -0500

    xml changes

--- app/src/main/java/com/rabotnickstudios/reza/SkillSection.java
+++ app/src/main/java/com/rabotnickstudios/reza/SkillSection.java
@@ -1,9 +1,12 @@
 package com.rabotnickstudios.reza;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SkillSection extends ActionBarActivity {
@@ -12,6 +15,15 @@ public class SkillSection extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skill_section);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ExperienceSection.class);
+                startActivity(i);
+            }
+        });
     }
 
 
